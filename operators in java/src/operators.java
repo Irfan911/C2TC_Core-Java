359ae992fdaa133ce3e67bfa338d50655ff6d90f
Irfan <Irfan@LAPTOP-T1G600LV>
2022-01-30 23:34:03 +0530

    first commit

---
+++ operators in java/src/operators.java
@@ -0,0 +1,25 @@
+
+public class operators {
+	public static void main(String[] args) {
+		// example 1
+		// int a=5,b=18;
+		// if((a=3)==b)
+		/// { System.out.println(++a);}
+		// else
+		// { System.out.println(++b); }
+//Example2
+
+		// int a=2;
+		// int b=5;
+		// int min=(a<b)?a:b;
+		// System.out.println(min);
+//Example3
+		/*
+		 * int a=10; int b=5; int c=20; System.out.println(a>b||a<c);
+		 * System.out.println(a>b|a<c); System.out.println(a>b||a++<c);
+		 * System.out.println(a); System.out.println(a>b|a++<c); System.out.println(a);
+		 */
+
+
+	}
+}
